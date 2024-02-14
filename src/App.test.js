c860c9a8e7749d5ef5f983c5d67d7c019091ff49
keshavagarwal670 <agarwalkeshav670@gmail.com>
2024-02-14 20:54:16 +0530

    Commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,95 +2,59 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders calculator with initial display 0', () => {
-  const { getByText } = render(<App />);
-  const displayElement = getByText(/0/i);
-  expect(displayElement).toBeInTheDocument();
+test('renders calculator with initial display of 0', () => {
+  const { getByDisplayValue } = render(<App />);
+  const display = getByDisplayValue('0');
+  expect(display).toBeInTheDocument();
 });
 
-test('clicking on number buttons updates the display', () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText('1'));
-  fireEvent.click(getByText('2'));
-  fireEvent.click(getByText('3'));
-  const displayElement = getByText(/123/i);
-  expect(displayElement).toBeInTheDocument();
-});
-
-test('perform addition operation correctly', () => {
-  const { getByText } = render(<App />);
+test('perform addition operation', () => {
+  const { getByText, getByDisplayValue } = render(<App />);
   fireEvent.click(getByText('1'));
   fireEvent.click(getByText('+'));
   fireEvent.click(getByText('2'));
   fireEvent.click(getByText('='));
-  const displayElement = getByText(/3/i);
-  expect(displayElement).toBeInTheDocument();
+  const display = getByDisplayValue('3');
+  expect(display).toBeInTheDocument();
 });
 
-test('perform subtraction operation correctly', () => {
-  const { getByText } = render(<App />);
+test('perform subtraction operation', () => {
+  const { getByText, getByDisplayValue } = render(<App />);
   fireEvent.click(getByText('5'));
   fireEvent.click(getByText('-'));
-  fireEvent.click(getByText('2'));
+  fireEvent.click(getByText('3'));
   fireEvent.click(getByText('='));
-  const displayElement = getByText(/3/i);
-  expect(displayElement).toBeInTheDocument();
+  const display = getByDisplayValue('2');
+  expect(display).toBeInTheDocument();
 });
 
-test('perform multiplication operation correctly', () => {
-  const { getByText } = render(<App />);
+test('perform multiplication operation', () => {
+  const { getByText, getByDisplayValue } = render(<App />);
   fireEvent.click(getByText('4'));
   fireEvent.click(getByText('*'));
   fireEvent.click(getByText('3'));
   fireEvent.click(getByText('='));
-  const displayElement = getByText(/12/i);
-  expect(displayElement).toBeInTheDocument();
+  const display = getByDisplayValue('12');
+  expect(display).toBeInTheDocument();
 });
 
-test('perform division operation correctly', () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText('8'));
+test('perform division operation', () => {
+  const { getByText, getByDisplayValue } = render(<App />);
+  fireEvent.click(getByText('9'));
   fireEvent.click(getByText('/'));
-  fireEvent.click(getByText('2'));
-  fireEvent.click(getByText('='));
-  const displayElement = getByText(/4/i);
-  expect(displayElement).toBeInTheDocument();
-});
-
-test('clear display when C button is clicked', () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText('5'));
-  fireEvent.click(getByText('C'));
-  const displayElement = getByText(/0/i);
-  expect(displayElement).toBeInTheDocument();
-});
-
-test('display Error when invalid expression is entered', () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText('5'));
-  fireEvent.click(getByText('/'));
-  fireEvent.click(getByText('0'));
-  fireEvent.click(getByText('='));
-  const displayElement = getByText(/Error/i);
-  expect(displayElement).toBeInTheDocument();
-});
-
-test('perform logarithm operation correctly', () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText('1'));
-  fireEvent.click(getByText('0'));
-  fireEvent.click(getByText('log'));
-  fireEvent.click(getByText('='));
-  const displayElement = getByText(/1/i);
-  expect(displayElement).toBeInTheDocument();
-});
-
-test('perform power operation correctly', () => {
-  const { getByText } = render(<App />);
-  fireEvent.click(getByText('2'));
-  fireEvent.click(getByText('pow'));
   fireEvent.click(getByText('3'));
   fireEvent.click(getByText('='));
-  const displayElement = getByText(/8/i);
-  expect(displayElement).toBeInTheDocument();
+  const display = getByDisplayValue('3');
+  expect(display).toBeInTheDocument();
+});
+
+
+
+
+test('clear display', () => {
+  const { getByText, getByDisplayValue } = render(<App />);
+  fireEvent.click(getByText('7'));
+  fireEvent.click(getByText('C'));
+  const display = getByDisplayValue('0');
+  expect(display).toBeInTheDocument();
 });
